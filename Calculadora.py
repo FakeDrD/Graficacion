4ef85255c024f217ddb802cd49fb960d048cc501
FakeDrD <cmjj3307@gmail.com>
2025-10-07 14:25:00 -0600

    first commit

---
+++ Calculadora.py
@@ -0,0 +1,9 @@
+# Solicita al usuario ingresar dos números
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+# Realiza la suma
+suma = num1 + num2
+
+# Muestra el resultado
+print("La suma es:", suma)
